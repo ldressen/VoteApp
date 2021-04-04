1896d34f8fd7a022ce452f427bfbd67b979a5f92
Luis Diniz Do Carmo <luis.do-carmo@alumni.fh-aachen.de>
2021-04-04 22:09:10 +0200

    Klassenname geändert zu "GUI"

--- VoteApp/app/src/main/java/com/example/voteapp/GUI.java
+++ VoteApp/app/src/main/java/com/example/voteapp/GUI.java
@@ -15,7 +15,7 @@ import android.widget.Button;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class GUI extends AppCompatActivity {
 
 
 
